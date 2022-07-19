c758b1334ce4f0e600f391fbb5f761c36ff937e8
antoniorand <tony.randalph@gmail.com>
2022-07-19 11:13:38 +0200

    deleted extra cout

--- src/systems/renderSystem.cpp
+++ src/systems/renderSystem.cpp
@@ -24,7 +24,6 @@ bool RenderSystem::update(sf::Time delta,entt::registry& engine){
     for(auto entity : view){
         auto& renderCMP = view.get<RenderComponent>(entity);
         window.draw(renderCMP.sprite);
-        std::cout << renderCMP.sprite.getTexture();
     }
 
     //////
